d7b16e8a41a4512db9764e35a31166cd23b94d98
JayLewiz <gony991@gmail.com>
2026-08-25 13:59:31 -0400

    commit

--- index.js
+++ index.js
@@ -2,6 +2,32 @@
 // service_rcxsp6h
 // cWo3rMKR9MIHVIVzV
 
+let isModalOpen = false
+let contrastToggle = false
+const scaleFactor = 1 / 20
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape")
+  const x = event.clientX * scaleFactor
+  const y = event.clientY * scaleFactor
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0
+    const boolInt = isOdd ? -1 : 1
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+  }
+}
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle
+  if (contrastToggle) {
+    document.body.classList += " dark-theme"
+  }
+  else {
+    document.body.classList.remove("dark-theme")
+  }
+}
+
 function contact(event) {
   event.preventDefault()
   const loading = document.querySelector('.modal__overlay--loading')
@@ -25,4 +51,13 @@ function contact(event) {
     })
 
 } 
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false
+    return document.body.classList.remove("modal--open")
+  }
+  isModalOpen = true
+  document.body.classList += " modal--open"
+}
   
